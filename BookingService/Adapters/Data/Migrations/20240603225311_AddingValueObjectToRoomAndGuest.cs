60a23bcd5b0faa02496724a046195843fa4e7bd6
BonoX23 <ricardo.bonos@gmail.com>
2024-06-03 20:13:03 -0300

    Introduzindo a camada de banco de dados

---
+++ BookingService/Adapters/Data/Migrations/20240603225311_AddingValueObjectToRoomAndGuest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class AddingValueObjectToRoomAndGuest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price_Currency",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price_Value",
+                table: "Rooms",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentId_DocumentType",
+                table: "Guests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentId_IdNumber",
+                table: "Guests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price_Currency",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "Price_Value",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentId_DocumentType",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentId_IdNumber",
+                table: "Guests");
+        }
+    }
+}
